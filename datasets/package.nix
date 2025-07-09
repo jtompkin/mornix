bee35e1a848482b918828f4ee9154da694b6d834
jtompkin <jtompkin-dev@pm.me>
2025-07-09 09:59:17 -0400

    fixed datasets package

--- datasets/package.nix
+++ datasets/package.nix
@@ -10,10 +10,20 @@ let
     "aarch64-darwin" = "mac";
   };
 in
-fetchurl rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "datasets";
   version = "v2";
-  executable = true;
-  url = "https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/${version}/${systemMap.${stdenv.system}}/${pname}";
-  hash = "sha256-C1n0jNQqRj7/UpTBe5ruHieaUtrNyxVav3WPKeO0rHk=";
-}
+  src = fetchurl {
+    url = "https://ftp.ncbi.nlm.nih.gov/pub/datasets/command-line/${finalAttrs.version}/${systemMap.${stdenv.system}}/${finalAttrs.pname}";
+    hash = "sha256-C1n0jNQqRj7/UpTBe5ruHieaUtrNyxVav3WPKeO0rHk=";
+    executable = true;
+  };
+  dontUnpack = true;
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/${finalAttrs.pname}
+  '';
+  meta = {
+    mainProgram = finalAttrs.pname;
+  };
+})
