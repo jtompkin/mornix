e15e53e882a17e0c351c453225e8845b6a29d94d
jtompkin <jtompkin-dev@pm.me>
2025-09-18 22:31:19 -0400

    change forAllSystems to accept system and pkgs arguments

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A collection of custom Nix packages/modules/overlays/treats";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,8 +24,7 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      forAllSystems = lib.genAttrs allSystems;
-      pkgsBySystem = forAllSystems (system: import nixpkgs { inherit system; });
+      forAllSystems = f: lib.genAttrs allSystems (system: f system nixpkgs.legacyPackages.${system});
       genModules =
         packages: type:
         lib.genAttrs packages (
@@ -47,11 +46,7 @@
     in
     {
       packages = forAllSystems (
-        system:
-        let
-          pkgs = pkgsBySystem.${system};
-        in
-        {
+        system: pkgs: {
           goclacker = pkgs.callPackage ./goclacker/package.nix { };
           datasets = pkgs.callPackage ./datasets/package.nix { };
           msedit = pkgs.callPackage ./msedit/package.nix { fenix = fenix.packages.${system}; };
@@ -70,7 +65,7 @@
         };
       };
       overlays = {
-        all = final: prev: self.packages.${prev.system};
+        all = final: prev: self.packages.${prev.stdenv.hostPlatform.system};
         default = self.overlays.all;
       };
     };
