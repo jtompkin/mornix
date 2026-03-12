0bda850ca776bc4376231df68295204acb84e5d5
jtompkin <jtompkin-dev@pm.me>
2026-03-12 12:37:18 -0400

    update update

--- update.sh
+++ update.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p nix-update -p fd -p ripgrep
-update_to_head=$(fd -t f package.nix packages -X rg -l _commit)
-n=$(echo $update_to_head | wc -w)
-i=1
-for pkg_path in $update_to_head; do
-    pkg="$(echo $pkg_path | cut -d'/' -f2)"
-    read -N 1 -p "[$i/$n] Updating $pkg... Commit changes? (Y/n) " choice
+pkgs_list="$(fd -t f -e nix package packages -X rg -l _commit)"
+mapfile update_to_head <<<"${pkgs_list}"
+n=${#update_to_head[@]}
+for ((i=0; i<n; i++)); do
+    pkg_path="${update_to_head[i]}"
+    pkg="$(echo "${pkg_path}" | cut -d'/' -f2)"
+    read -r -N 1 -p "[$((i + 1))/${n}] Updating ${pkg}... Commit changes? (Y/n) " choice
     echo
-    commit_arg="--commit"
-    if [[ "$choice" == n ]]; then
-        commit_arg="--write-commit-message .update-msgs/"$pkg""
+    commit_args=(--commit)
+    if [[ "${choice}" == n ]]; then
+        commit_args=(--write-commit-message ".update-msgs/${pkg}")
     fi
-    nix-update --flake --build --version=branch $@ $commit_arg "$pkg"
-    i=$(( i + 1 ))
+    nix-update --flake --build --version=branch "$@" "${commit_args[@]}" "${pkg}"
 done
-echo "Done updating packages to HEAD"
